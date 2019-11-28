4a5b44b1721f2e96a09ae0feff7ca7decf50d28b
florianbazerque <florian.bazerque@orange.fr>
2019-11-28 14:36:15 +0100

    gaz lenght correction

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
-#from flask_mysqldb import MySQL
-import csv, yaml
-
+import csv
 
 app = Flask(__name__)
 
-#Configuration base de données
-#db = yaml.load(open('settings.yaml'))
-#app.config['MYSQL_HOST'] = db['MYSQL_HOST']
-#app.config['MYSQL_USER'] = db['MYSQL_USER']
-#app.config['MYSQL_PASSWORD'] = db['MYSQL_PASSWORD']
-#app.config['MYSQL_DB'] = db['MYSQL_DB']
-
-#mysql = MySQL(app)
-
 @app.route('/')
 def home():
-    try:
-	    cur = mysql.connection.cursor()
-    except:
-	    print('oups')
-    
     gaz = parse_from_csv()
     return render_template("home.html", gaz = gaz)
 
@@ -29,7 +13,7 @@ def save_gazouille():
 	if request.method == 'POST':
 		print(request.form)
 		dump_to_csv(request.form)
-		return redirect(url_for('timeline'))
+		return redirect(url_for('home'))
 		#return "OK"
 	if request.method == 'GET':
 		return render_template('formulaire.html')
@@ -37,8 +21,6 @@ def save_gazouille():
 @app.route('/timeline', methods=['GET'])
 def timeline():
 	gaz = parse_from_csv()
-	for g in gaz:
-		g = g[:75]
 	return render_template("timeline.html", gaz = gaz)
 
 @app.after_request
@@ -52,11 +34,13 @@ def parse_from_csv():
 	with open('./gazouilles.csv', 'r') as f:
 		reader = csv.reader(f)
 		for row in reader:
-			gaz.append({"user":row[0], "text":row[1]})
+			if len(row[1]) < 280:
+				gaz.append({"user":row[0], "text":row[1]})
 	return gaz
 
 def dump_to_csv(d):
-	donnees = [d["user-name"],d["user-text"] ]
-	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
-		writer = csv.writer(f)
-		writer.writerow(donnees)
+	if len(d["user-text"]) < 280:
+		donnees = [d["user-name"],d["user-text"] ]
+		with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+			writer = csv.writer(f)
+			writer.writerow(donnees)
